2ca0566de61fbcb82eb44e1ca81c8bcddb12503e
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-12-07 17:49:47 +0100

    updates

--- Poster_Templates/Flo_Ben_Poster/script.js
+++ Poster_Templates/Flo_Ben_Poster/script.js
@@ -1,33 +1,57 @@
 //TODO: incomingimage 0 - 0.5, outgoingimage 0.5 - 1
 //TODO: lerp in and out of incoming and outgoing image
+//TODO: higher quality png
 
 let rotationHistory = [];
 let images = [];
 let font;
 let aspectRatio = 1.375;
-let transitionInScale = 0;
-let transitionOutScale = 0.5
-
 
 let previousCounter = -1;
-let transitionInIncrement = 0.05;
-let transitionOutIncrement = 0.09
+let increment = 0.05;
 let transitionFlag = true;
 
-let outgoingImage;
 let incomingImage;
+let outgoingImage;
+let startInScale
+let startOutScale
 
-// let transitionEndpoints = {
-//     no1: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no2: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no3: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no4: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no5: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no6: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no7: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no8: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-//     no9: {endX: 0.5, endY: 0.5, endScale: 5.5, endRotation: 0.01},
-// }
+let transitionInScale = 0;
+let transitionOutScale = 0.6;
+let transitionInIncrement = 0.06;
+let transitionOutIncrement = 0.09;
+let incomingRotation = 0;
+
+let transitionInProgress
+let transitionOutProgress
+
+let anchorPoints = [
+    // 0
+    { x: -0.34, y: 0 },
+    // 1
+    { x: 0.25, y: 0.2 },
+    // 2 
+    { x: -0.25, y: -0.1 },
+    // 3 
+    { x: -0.32, y: -0.33 },
+    // 4 
+    { x: 0.2, y: -0.3 },
+    // 5 
+    { x: 0.32, y: -0.32},
+    // 6 
+    { x: 0.22, y: -0.23},
+    // 7 
+    // { x: 0.05, y: 0.29 },
+    { x: -0.29, y: -0.29 },
+    // 8 
+    { x: 0.2, y: 0.2 },
+    // 9 
+    { x: -0.25, y: -0.25}
+];
+
+let currentOutgoingAnchor = { x: 0.5, y: 0.5 }; 
+
+
 
 function preload() {
     for (let i = 0; i < 10; i++) {
@@ -38,58 +62,86 @@ function preload() {
 function setup() {
     createCanvas(poster.getWindowWidth(), poster.getWindowHeight());
     poster.setup(this, "models/movenet/model.json");
-
-    // incomingImage = images[0];
-    // outgoingImage = images[1]
 }
 
 function draw() {
-    // background(poster.getCounter() % 2 === 0 ? 0 : 255);
-    // background(poster.getCounter() % 2 === 0 ? 255 : 0);
-     background(50)
+    background(poster.getCounter() % 2 === 0 ? 255 : 0);
+    // background(50);
 
-     // first assigning the images
-     outgoingImage = images[poster.getCounter()];
-     incomingImage = images[poster.getCounter() - 1];
-     if (incomingImage === undefined) {
-         incomingImage = images[9];
-     }
+    let outgoingIndex = poster.getCounter();
+    let incomingIndex = (poster.getCounter() - 1 + images.length) % images.length;
 
+    outgoingImage = images[outgoingIndex];
+    incomingImage = images[incomingIndex];
 
-
+    let targetOutgoingAnchor = anchorPoints[outgoingIndex];
 
     if (poster.getCounter() !== previousCounter) {
         transitionInScale = 0;
-        transitionOutScale = 0.5
-        //this stupid thing fixes the size increase
-        transitionInIncrement = 0.05;
-        transitionOutIncrement = 0.1;
+        transitionOutScale = 0.6;
+        transitionInIncrement = 0.03;
+        transitionOutIncrement = 0.2;
+
+        incomingRotation = PI / 2; 
+        currentOutgoingAnchor = { x: 0, y: 0};
         previousCounter = poster.getCounter();
+
+        transitionInProgress = 0; 
+        transitionOutProgress = 0; 
     }
 
-    let targetScale = 0.6
+    let targetInScale = 0.6;
+    let targetOutScale = 4.5
 
-    transitionInScale += transitionInIncrement;
-    transitionOutScale += transitionOutIncrement
+    // fast speed
+    // transitionInScale = lerp(transitionInScale, targetInScale, transitionInIncrement*4);
+    // transitionOutScale = lerp(transitionOutScale, targetOutScale, transitionOutIncrement*0.5);
 
-    if (transitionInScale < 0.6) {
-        transitionInIncrement = 0.05;
-        transitionOutIncrement = 0.1
+    // middle speed
+    // transitionInScale = lerp(transitionInScale, targetInScale, transitionInIncrement*3);
+    // transitionOutScale = lerp(transitionOutScale, targetOutScale, transitionOutIncrement*0.3);
+
+    // slow speed
+    transitionInScale = lerp(transitionInScale, targetInScale, transitionInIncrement*1.55);
+    transitionOutScale = lerp(transitionOutScale, targetOutScale, transitionOutIncrement*0.25);
+    
+
+    // out easing tests
+    // transitionOutProgress += 0.04;
+    // transitionOutProgress = constrain(transitionOutProgress, 1, 3);
+    // transitionOutScale = transitionOutProgress * transitionOutProgress * targetOutScale/8;
+
+
+    // works with lerp
+    currentOutgoingAnchor.x = lerp(currentOutgoingAnchor.x, targetOutgoingAnchor.x, transitionOutIncrement);
+    currentOutgoingAnchor.y = lerp(currentOutgoingAnchor.y, targetOutgoingAnchor.y, transitionOutIncrement);
+
+
+    // works with other easing
+    // currentOutgoingAnchor.x = lerp(currentOutgoingAnchor.x, targetOutgoingAnchor.x, transitionOutIncrement/3);
+    // currentOutgoingAnchor.y = lerp(currentOutgoingAnchor.y, targetOutgoingAnchor.y, transitionOutIncrement/3);
+
+    if (transitionInScale < targetInScale) {
+        incomingRotation = lerp(incomingRotation, 0, 0.16);
     } else {
-        transitionInIncrement = 0;
-        transitionOutIncrement = 0
+        incomingRotation = 0;
     }
+
 
     push();
         imageMode(CENTER);
-        // let anchorX = width / 2
-        // let anchorY = height / 2
-        // translate(anchorX, anchorY);
+        translate(
+            width / 2 - (currentOutgoingAnchor.x) * width * transitionOutScale,
+            height / 2 - (currentOutgoingAnchor.y) * height / aspectRatio * transitionOutScale
+        );
+        image(outgoingImage, 0, 0, width * transitionOutScale, (height / aspectRatio) * transitionOutScale);
+    pop();
+
+    push();
+        imageMode(CENTER);
         translate(width / 2, height / 2);
-        let outgoingScale = transitionInScale;
-        let incomingScale= transitionOutScale
-        image(outgoingImage, 0, 0, width * incomingScale, (height / aspectRatio) * incomingScale);
-        image(incomingImage, 0, 0, width * outgoingScale, (height / aspectRatio) * outgoingScale);
+        rotate(-incomingRotation);
+        image(incomingImage, 0, 0, width * transitionInScale, (height / aspectRatio) * transitionInScale);
     pop();
 
     poster.posterTasks();
@@ -98,10 +150,3 @@ function draw() {
 function windowScaled() {
     textSize(10 * poster.vw);
 }
-
-
-/*
-   let difference = outgoingScale - endpoints[0].endScale 
-   difference *= 0.1;
-    outgoingScale += difference;
-*/
